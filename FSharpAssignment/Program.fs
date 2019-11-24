4698b1acc700cdc57926671c0e69a0af14191056
betteredition <jesperenemark@hotmail.com>
2019-11-24 16:29:27 +0100

    made minus function

--- FSharpAssignment/Program.fs
+++ FSharpAssignment/Program.fs
@@ -4,7 +4,7 @@
 
 let list1 = [ 3;4;5;12;13;15 ] 
 let list2 = [ 1;1;1;1;2;2;2;3;3;3;3;3;4;4;4;4;4;4;5;5;6;6;7;7;7;7;7;7;7;8;8;8;8;8;9;9;9;9;9;10; ]
-let number = 3
+
 // 4.11 - 1. Find Occurrences 
 let rec count item = function
     | [] -> 0
@@ -24,29 +24,28 @@ let rec isMember num = function
 
 let rec compare list2 = function
   | [] -> []
-  | x :: xs -> 
+  | x :: xs ->
       let rest = compare xs list2
       if isMember x list2 then x::rest
       else rest
   
 
 // 4.11 - 4. Plus
-let plus (list1, list2) =
-    let rec plus' list resultlist = function
-      | [] -> resultlist
-      | x::xs -> insert resultlist x |> plus'
-    plus' list2 list1
+let rec plus list resultlist =
+        match list with
+        | [] -> resultlist
+        | x::xs -> insert x resultlist |> plus xs
+
 
  // 4.11 - 5. Minus
-let minus (minuendList, subtrahendList) =
-    let rec removeItem list itemToRemove resultList =
+let rec removeItem list itemToRemove resultList =
         match list with
         | [] -> resultList
-        | x::[] when x = itemToRemove -> resultList 
+        | x::[] when x = itemToRemove -> resultList
         | x::xs when x = itemToRemove -> resultList @ xs
         | x::xs -> resultList @ [x] @ removeItem xs itemToRemove resultList
-    let rec removeList listToRemove resultlist =
-        match listToRemove with
-        | [] -> resultlist
-        | x::xs -> removeList xs (removeItem resultlist x [])
-    removeList subtrahendList minuendList
+
+let rec removeList listToRemove itemsToBeRemoved =
+    match listToRemove with
+    | [] -> itemsToBeRemoved
+    | x::xs -> removeList xs (removeItem itemsToBeRemoved x [])
